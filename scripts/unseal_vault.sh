6b6bdd3d0606701cb4d6872ea5c8edbc3180f769
Shaun Taheri <shaun@advancedtelematic.com>
2017-12-07 12:25:53 +0100

    Fix advertised kafka hostname
    (cherry picked from commit d2cdcf5c1123ff8cffd85864d31458eae7f8ae3c)

--- scripts/unseal_vault.sh
+++ scripts/unseal_vault.sh
@@ -5,17 +5,19 @@ set -euo pipefail
 
 init_key=${INIT_KEY:-/init.key}
 
+vault init -check && exit 0
 vault init -key-shares=1 -key-threshold=1 > "${init_key}"
-vault init -check
-unseal_key=$(awk -F': ' '/^Unseal Key 1/{e=0; print $2; exit} {e=1} END{exit e}' "${init_key}")
+
 root_key=$(awk -F': ' '/^Initial Root Token/{e=0; print $2; exit} {e=1} END{exit e}' "${init_key}")
+unseal_key=$(awk -F': ' '/^Unseal Key 1/{e=0; print $2; exit} {e=1} END{exit e}' "${init_key}")
+vault unseal "${unseal_key}"
 export VAULT_TOKEN="$root_key"
 
-vault unseal "${unseal_key}"
 vault policy-write tuf - <<END
 path "ota-tuf/keys/*" {
   policy = "write"
 }
 END
+
 vault mount -path /ota-tuf/keys generic
 vault token-create -policy="tuf" -period="72h"

bcd8dc50fbcf209f23e6eee7182b18730bf29be3
Vladimir Koshelev <vladimir@advancedtelematic.com>
2018-01-02 13:27:19 +0100

    Register a device and auth it with nginx

--- scripts/genclient.sh
+++ scripts/genclient.sh
@@ -4,6 +4,9 @@ set -uex
 
 export SERVERNAME=${SERVERNAME:-ota.ce}
 export DEVICE_ID=${1}
+readonly KUBE_IP=$(minikube ip)
+export DEVICE_UUID=$(uuidgen)
+readonly REGISTRY_HOST=$(kubectl get ingress -o jsonpath --template='{.items[?(@.metadata.name=="device-registry")].spec.rules[0].host}')
 readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../${SERVERNAME}
 
 openssl ecparam -genkey -name prime256v1 | openssl ec -out ${DIR}/${DEVICE_ID}.key
@@ -12,3 +15,5 @@ openssl x509 -req -days 365 -extfile client.ext -in ${DIR}/${DEVICE_ID}.csr -CAk
 cat ${DIR}/${DEVICE_ID}.crt ${DIR}/${DEVICE_ID}.crt > ${DIR}/${DEVICE_ID}.chain.pem
 
 openssl x509 -in ${DIR}/${DEVICE_ID}.crt -text -noout
+
+http PUT http://${KUBE_IP}/api/v1/devices "Host:${REGISTRY_HOST}" deviceUuid="${DEVICE_UUID}" deviceId=${DEVICE_ID} deviceName=${DEVICE_ID} deviceType=Other credentials=@${DIR}/${DEVICE_ID}.crt
